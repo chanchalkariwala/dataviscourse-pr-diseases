529d09208319bbbb1e4f0462b5026e1f69d04166
Sheetal Krishna <u1135144@utah.edu>
2017-11-10 22:17:53 -0700

    SMJK commit 2

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,26 @@ d3.csv("data/expensebudget.csv", function (error, expenseDetails) {
     let releaseChart = new ReleaseChart(expenseDetails);
     
 });
+
+d3.csv("public/data/budgetRevenueYearChartMovieDetails.csv", function(d) {
+  d.budget = +d.budget;
+  d.revenue = +d.revenue;
+  d.year = +d.year;
+  return d;
+}, function(error, movieDetails) {
+    let budRev=""
+  if (error) throw error;
+      d3.csv("public/data/budgetRevenueYearChart.csv", function(d) {
+      d.budget = +d.budget;
+      d.revenue = +d.revenue;
+      d.year = +d.year;
+      return d;
+    }, function(error, budgetRevenue) {
+      if (error) throw error;
+        budRev=budgetRevenue;
+        let budRevYearChart = new budgetRevenueYearChart(budRev,movieDetails);
+        budRevYearChart.update();
+    });
+
+         
+});
